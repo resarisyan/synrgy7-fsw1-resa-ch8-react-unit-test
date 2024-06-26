c53fc957811293145bba23c5148fec31d1f345c4
resarisyan <resarisyan77@gmail.com>
2024-06-26 21:35:40 +0700

    fix: change backend url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9999/api/v1/',
+  baseURL: 'https://certain-kevina-resa-279afa0d.koyeb.app/api/v1/',
 });
